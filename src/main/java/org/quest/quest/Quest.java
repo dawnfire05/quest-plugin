45345d2aff289f59b31454fcac0a5c9ae25224ce
dawnfire05 <dawnfire05@naver.com>
2024-11-11 00:55:16 +0900

    feat: build

--- src/main/java/org/quest/quest/Quest.java
+++ src/main/java/org/quest/quest/Quest.java
@@ -2,13 +2,15 @@ package org.quest.quest;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class Quest extends JavaPlugin {
 
     @Override
     public void onEnable() {
         getLogger().info("Quest Plugin has been enabled!");
         QuestManager questManager = new QuestManager();
-        getCommand("quest").setExecutor(new QuestCommand(questManager));
+        Objects.requireNonNull(getCommand("quest")).setExecutor(new QuestCommand(questManager));
         getServer().getPluginManager().registerEvents(new QuestListener(questManager), this);
     }
 
